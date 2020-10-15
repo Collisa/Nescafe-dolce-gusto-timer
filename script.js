33063a35f7edce7984a1cd01290cb6e721a78caa
Lisa Collignon <68648800+LisaCollignon@users.noreply.github.com>
2020-10-15 21:56:46 +0200

    refresh and timer added

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let lineFirst = document.querySelector("div.line.first");
-let lineTwo = document.querySelector("div.line.two");
-let lineThree = document.querySelector("div.line.three");
-let lineFour = document.querySelector("div.line.four");
-let lineFive = document.querySelector("div.line.five");
-let lineSix = document.querySelector("div.line.six");
-let lineLast = document.querySelector("div.line.last");
+const lineFirst = document.querySelector("div.line.first");
+const lineTwo = document.querySelector("div.line.two");
+const lineThree = document.querySelector("div.line.three");
+const lineFour = document.querySelector("div.line.four");
+const lineFive = document.querySelector("div.line.five");
+const lineSix = document.querySelector("div.line.six");
+const lineLast = document.querySelector("div.line.last");
 const stopSign = document.querySelector("div.container.lines");
+const refresh = document.querySelector("svg")
 
 const startButton = document.querySelector("button.start");
 
@@ -93,23 +94,42 @@ function changeInnerText() {
     stopSign.classList.add("red");
 }
 
+function changeStartText(num) {
+    num--;
+    interval = setInterval(function(){
+        startButton.innerHTML = num--;
+    }, 1000);
+}
+
+
+function deleteStart() {
+    clearInterval(interval);
+    startButton.innerHTML = "Enjoy!";
+}
+
 function timerFunctionality() {
     console.log(lineFirst.classList);
     switch (index) {
                 case 0:
                     setTimeout(function(){deleteColor(lineFirst)}, 8000);
                     setTimeout(function(){changeInnerText()}, 8100);
+                    changeStartText(8);
+                    setTimeout(function(){deleteStart()}, 8100);
                     break;
                 case 1:
                     setTimeout(function(){deleteColor(lineTwo)}, 5000);
                     setTimeout(function(){deleteColor(lineFirst)}, 10000);
                     setTimeout(function(){changeInnerText()}, 10100);
+                    changeStartText(10);
+                    setTimeout(function(){deleteStart()}, 10100);
                     break;
                 case 2:
                     setTimeout(function(){deleteColor(lineThree)}, 5000);
                     setTimeout(function(){deleteColor(lineTwo)}, 10000);
                     setTimeout(function(){deleteColor(lineFirst)}, 15000);
                     setTimeout(function(){changeInnerText()}, 15100);
+                    changeStartText(15);
+                    setTimeout(function(){deleteStart()}, 15100);
                     break;
                 case 3:
                     setTimeout(function(){deleteColor(lineFour)}, 5250);
@@ -117,6 +137,8 @@ function timerFunctionality() {
                     setTimeout(function(){deleteColor(lineTwo)}, 15750);
                     setTimeout(function(){deleteColor(lineFirst)}, 21000);
                     setTimeout(function(){changeInnerText()}, 21100);
+                    setTimeout(function(){deleteStart()}, 21100);
+                    changeStartText(21);
                     break;
                 case 4:
                     setTimeout(function(){deleteColor(lineFive)}, 4800);
@@ -125,6 +147,8 @@ function timerFunctionality() {
                     setTimeout(function(){deleteColor(lineTwo)}, 19200);
                     setTimeout(function(){deleteColor(lineFirst)}, 24000);
                     setTimeout(function(){changeInnerText()}, 24100);
+                    setTimeout(function(){deleteStart()}, 24100);
+                    changeStartText(24);
                     break;
                 case 5:
                     setTimeout(function(){deleteColor(lineSix)}, 4667);
@@ -134,6 +158,8 @@ function timerFunctionality() {
                     setTimeout(function(){deleteColor(lineTwo)}, 23335);
                     setTimeout(function(){deleteColor(lineFirst)}, 28000);
                     setTimeout(function(){changeInnerText()}, 28100);
+                    setTimeout(function(){deleteStart()}, 28100);
+                    changeStartText(28);
                     break;
                 case 6:
                     setTimeout(function(){deleteColor(lineLast)}, 5429);
@@ -144,9 +170,16 @@ function timerFunctionality() {
                     setTimeout(function(){deleteColor(lineTwo)}, 32574);
                     setTimeout(function(){deleteColor(lineFirst)}, 38000);
                     setTimeout(function(){changeInnerText()}, 38100);
+                    setTimeout(function(){deleteStart()}, 38100);
+                    changeStartText(38);
                     break;
     }
 }
 
+function reNew() {
+    window.location.reload();    
+}
+
 window.addEventListener("click", changeColor);
 startButton.addEventListener("click", timerFunctionality);
+refresh.addEventListener("click", reNew);
